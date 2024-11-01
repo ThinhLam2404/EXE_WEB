75ee750d2b967e3734f7a91373fd048c829a4726
ThinhLam <thinhlam240499@gmail.com>
2024-11-01 21:42:21 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import newLogo from "./assets/images/logo-web.jpg";
 
 function App() {
   return <Outlet></Outlet>;
