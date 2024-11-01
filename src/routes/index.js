8c560df2c63adf4bae674d8f0a4f096974e905d8
Tran Thanh Duy <thanhduyct03@gmail.com>
2024-11-01 17:34:42 +0700

    fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,17 @@ import {
 } from "react-router-dom";
 import App from "../App";
 import Layout from "../components/Layout";
-import Home from "../components/Home";
+import Calculate from "../components/Calculate";
+// import Home from "../components/Home";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Layout />}>
         {/* <Route index element={<Home />}></Route> */}
         {/* <Route path="about" element={<h1>About Us</h1>} /> */}
+      </Route>
+      <Route path="/cal" element={<Calculate/>}>
+
       </Route>
     </Route>
   )
